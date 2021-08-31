381c47f3f82bf8933c54ba0c077d5c11bd7f11a8
Peanut <2857177614@qq.com>
2021-09-01 00:08:41 +0800

    顺序查找

--- main.cpp
+++ main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <time.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int main()
+{
+    const long N = 1e9;
+    long* list = new long[N];
+    for (long i = 0; i < N; ++i) {
+        *(list + i) = i;
+    }
+    // Ë³Ðò²éÕÒ
+//    long target = -1;
+//    cin >> target;
+    long target = 1e9-1;
+    time_t start,end;
+    start =time(NULL);
+    for (long i = 0; i < N; ++i) {
+        if (list[i] == target)
+            cout << "Get it ! " << i;
+    }
+    end =time(NULL);
+    printf("time=%d\n",difftime(end,start));
 }
