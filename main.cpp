74e1a77712d51adf8604e8c719250efb0c4965d9
peanut <2857177614@qq.com>
2021-09-12 15:31:40 +0800

    test

--- main.cpp
+++ main.cpp
@@ -1,26 +1,7 @@
-#include "list/LinkedList.cpp"
-#include "list/ArrayList.cpp"
-#include "list/tool.h"
 #include <iostream>
 
-
-
-int main() {
-    List<int>* list1 = new LinkedList<int>;
-    List<int>* list2 = new LinkedList<int>;
-    int a[] = {6,7,4,2,9,-1, 888, 777};
-    int b[] = {2,8,666,9,1};
-    initList(list1, a, sizeof(a)/sizeof(int));
-    initList(list2, b, sizeof(b)/sizeof(int));
-    showList(list1);
-    showList(list2);
-    std::cout << "-----------------" << std::endl;
-    sortList(list1);
-    sortList(list2);
-    showList(list1);
-    showList(list2);
-    std::cout << "==================" << std::endl;
-    LinkedList<int>* list = mergeList(list1, list2);
-    showList(list);
-
+int main()
+{
+    std::cout << "Hello" << std::endl;
+    return 0;
 }
