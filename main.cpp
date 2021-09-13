89964631c6d95ace7aee14a59886e162dd790a00
peanut <2857177614@qq.com>
2021-09-14 00:32:28 +0800

    gbk编码

--- main.cpp
+++ main.cpp
@@ -1,24 +1,11 @@
-#include "list/LinkedList.cpp"
-#include "list/ArrayList.cpp"
-#include "list/tool.h"
-#include <iostream>
+#include<iostream>
+#include<string>
+using namespace std;
 
 int main() {
-    List<int>* list1 = new LinkedList<int>;
-    List<int>* list2 = new LinkedList<int>;
-    int a[] = {6,7,4,2,9,-1, 888, 777};
-    int b[] = {2,8,666,9,1};
-    initList(list1, a, sizeof(a)/sizeof(int));
-    initList(list2, b, sizeof(b)/sizeof(int));
-    showList(list1);
-    showList(list2);
-    std::cout << "-----------------" << std::endl;
-    sortList(list1);
-    sortList(list2);
-    showList(list1);
-    showList(list2);
-    std::cout << "==================" << std::endl;
-    LinkedList<int>* list = mergeList(list1, list2);
-    showList(list);
-
+    string msg;
+    cout << "ÇëÊäÈë" << endl;
+    cin >> msg;
+    cout << "»ØÉù£º" << msg << endl;
+    return 0;
 }
