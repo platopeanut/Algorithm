d110265fdd6dd942dd739f6ffbbcff2ae6151d84
Peanut <2857177614@qq.com>
2021-10-12 20:35:58 +0800

    实现Heap

--- dataStructures/heap/Heap.cpp
+++ dataStructures/heap/Heap.cpp
@@ -4,10 +4,14 @@
 template<typename E>
 void Heap<E>::siftDown(int pos) {
     // 排列成pos为根节点的堆
+    // 使用判断叶节点为终止条件来替代递归
     while (!isLeaf(pos)) {
+        // 注意这里的lc必存在，因为分支节点必有子节点，又因为堆是完全二叉树
         int lc = leftChild(pos); // pos的left child下标
         int rc = rightChild(pos); // pos的right child下标
+        // rc 右子节点可能不存在需要检验
         if (rc < num && Compare<E>::prior(heap[rc], heap[lc])) lc = rc;
+        // 此时lc指向左右节点大的一个
         if (Compare<E>::prior(heap[pos], heap[lc])) return;
         Compare<E>::swap(heap, pos, lc);
         pos = lc;
@@ -19,6 +23,7 @@ void Heap<E>::insert(const E &item) {
     if (num >= maxSize) throw StringException("Heap<E>::insert==>Heap is full");
     int curr = num++;
     heap[curr] = item;
+    // 一直往上走
     while (curr != 0 && Compare<E>::prior(heap[curr], heap[parent(curr)])) {
         Compare<E>::swap(heap, curr, parent(curr));
         curr = parent(curr);
@@ -39,11 +44,36 @@ E Heap<E>::remove(int pos) {
     if (pos == num - 1) num --;
     else {
         Compare<E>::swap(heap, pos, --num);
+        // 先上走
         while (pos != 0 && Compare<E>::prior(heap[pos], heap[parent(pos)])) {
             Compare<E>::swap(heap, pos, parent(pos));
             pos = parent(pos);
         }
+        // 后下沉
         if (num != 0) siftDown(pos);
     }
     return heap[num];
+}
+
+
+template<typename E>
+void Heap<E>::show() {
+    std::cout << "Heap[";
+    for (int i = 0; i < num; ++i) {
+        std::cout << heap[i] << " ";
+    }
+    std::cout << "]" << std::endl;
+}
+
+template<typename E>
+void Heap<E>::heap_sort() {
+    std::cout << "Heap Sort: ";
+    while (true) {
+        try {
+            std::cout << removeFirst() << " ";
+        } catch (StringException& e) {
+            break;
+        }
+    }
+    std::cout << std::endl;
 }
--- dataStructures/heap/Heap.h
+++ dataStructures/heap/Heap.h
@@ -29,7 +29,11 @@ private:
 
 public:
     // 使用数组创建一个堆
-    Heap(E* heap, int num, int maxSize): heap(heap), num(num), maxSize(maxSize) {buildHeap();}
+    Heap(E* heap, int num, int maxSize = 1024): num(num), maxSize(maxSize) {
+        this->heap = new E[maxSize];
+        for (int i = 0; i < num; ++i) this->heap[i] = heap[i];
+        buildHeap();
+    }
     int size() const {return num;}
     // 根据完全二叉树定理，没有越界检查
     bool isLeaf(int pos) const { return pos >= num/2 && pos < num; }
@@ -37,20 +41,20 @@ public:
     int rightChild(int pos) const {return 2*pos + 2;}
     int parent(int pos) const {return (pos - 1) / 2;}
     void buildHeap() {
-        // num/2-1是完全二叉树中最后一个分支节点下标
+        // num/2-1是完全二叉树中最后一个分支节点下标,或者写成parent(size-1)
+        // 为啥要从最后一个分支节点开始？因为单个叶节点可以认为是已经排列好的堆
         for (int i = num/2-1; i >= 0; i--) siftDown(i);
     }
 
+    // 插入一个新节点
     void insert(const E& item);
+    // 删除堆根节点
     E removeFirst();
+    // 根据位置删节点
     E remove(int pos);
 
-    void show() {
-        for (int i = 0; i < num; ++i) {
-            std::cout << heap[i] << " ";
-        }
-        std::cout << std::endl;
-    }
+    void show();
+    void heap_sort();
 };
 
 
