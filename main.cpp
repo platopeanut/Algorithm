163eaf03e0d44ca7a6d8617f179f362ccc4f18c8
Peanut <2857177614@qq.com>
2021-11-08 23:27:40 +0800

    nice

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 int main()
 {
-    std::cout << "hello world!" << std::endl;
+    int sum = 0;
+    for (int i = 0; i < 100; ++i) sum += i;
+    std::cout << "sum: " << sum << std::endl;
     return 0;
 }
