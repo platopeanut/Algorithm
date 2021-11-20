04d47f8a8634c08c7d677f8da4e0d9029e014b2c
Peanut <2857177614@qq.com>
2021-11-20 16:49:35 +0800

    优化了cmakelists

--- main.cpp
+++ main.cpp
@@ -1,26 +1,33 @@
-/**
- *  对 Dijkstra 算法进行测试
- */
-#include "./dataStructures/graph/MatrixGraph.h"
-#include "./algorithm/graph/graphs.h"
+#include <iostream>
+#include "./dataStructures/list/List.h"
+#include "./dataStructures/list/LinkedList.cpp"
 
-/**
- *  加载图11.16
- */
-void load(Graph* graph) {
-    graph->setEdge(0,1,10);
-    graph->setEdge(0,2,3);
-    graph->setEdge(0,3,20);
-    graph->setEdge(2,1,2);
-    graph->setEdge(1,3,5);
-//    graph->setEdge(3,4,11);
-//    graph->setEdge(2,4,15);
+template <typename E>
+void showList(List<E>* list) {
+    std::cout << "List[";
+    list->moveToStart();
+    for (int i = 0; i < list->length(); ++i) {
+        std::cout << list->getValue() << ", ";
+        list->next();
+    }
+    std::cout << "]" << std::endl;
 }
 
-int main() {
-    Graph* graph = new MatrixGraph(5);
-    load(graph);
-    Dijkstra(graph, 0);
-    delete graph;
+int main()
+{
+    List<int>* list = new LinkedList<int>;
+    list->append(2);
+    list->append(23);
+    list->append(15);
+    list->append(5);
+    list->append(9);
+    list->moveToStart();
+    showList(list);
+    // remove 15
+    list->moveToPos(2);
+    list->remove();
+    // show
+    showList(list);
+    delete list;
     return 0;
 }
